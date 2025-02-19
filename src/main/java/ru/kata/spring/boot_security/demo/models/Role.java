7154f4b3ca0bbaebbbe427984df9ef2d4bfd0f06
Arthur <peace_a99@mail.ru>
2025-02-19 09:00:41 +0200

    It has been changed

--- src/main/java/ru/kata/spring/boot_security/demo/models/Role.java
+++ src/main/java/ru/kata/spring/boot_security/demo/models/Role.java
@@ -10,7 +10,7 @@ public class Role implements GrantedAuthority {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Long id;
 
     @Column(unique = true, nullable = false)
     private String value;
@@ -27,11 +27,11 @@ public class Role implements GrantedAuthority {
         return value;
     }
 
-    public Integer getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(Integer id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
